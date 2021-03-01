3ca33c15fc258153068f04b7ad1d7e97fe3c2bb5
Varun Deva <varundev23@gmail.com>
2021-03-01 19:32:57 +0530

    bug fix and improvements

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from readFile import *
 from uuid import uuid4
-
+import helpTexts
 # Enable logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
@@ -12,37 +12,9 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-helpText = f'''This Bot🤖 will send you Timetable of your course
-
-<b>How to Get All Timetable of my Course?</b>
-First Send /start command to bot🤖
-After that follow instructions
-1️⃣ Select Your University 🎓
-2️⃣ Select Your Semester 📚
-3️⃣ Select Your Course 📖
-✅ You'll Receive Timetables
-
-<b>How to Get Single Subject Date and Time?</b>
-To get perticular subject date and time use /qpcode command
-
-1️⃣Send /qpcode with your subject QPCode
-    EX - /qpcode 12345
-    type /qpcode give a single space then type QPCode and send
-2️⃣This will reply you Date and Time of that Subject
-✅Done
-
-If you have any question contact us by using /contact command[You'll get contact details]
-
-➡️ Kindly Report if you have any issues regarding 🤖bot as well as the Data 🤖bot returned
-➡️ Don't follow blindly Bot🤖 given data kindly cross verify with the timetable in university website🌐
-'''
-
 
 def help_command(update, context):
     update.message.reply_html(helpText)
-
-
-footer = f"<b>🤝All The Best for your Exams📚</b>\n<b>👏Do Well in All the Exams 📚</b>\n\n\n\n🔗Connect With Us\n\n\nFor Study Materials 📖 Check<a href='https://bustudymate.in'> BUStudymate Blog</a>\n\nFor Instant Updates📢 Follow On <a href = 'https://instagram.com/bustudymate'>Instagram🖼️</a>\n\nFor 👩‍💻Discussion/Q & A⁉️ - <a href='https://forum.bustudymate.in'>Join BUForum</a>\n\nFeedbacks👏/ Report❌ Email ✉️ - admin@bustudymate.in\n\nDonate💵 to Run the Service - @bustudymate4u"
 
 
 def createUniversityKeyboard():
@@ -85,7 +57,7 @@ def sendTimeTable(context, update):
             dataString = ""
     if(dataString):
         dataList.append(dataString)
-    print("NUmber of Messsage" + str(len(dataString)))
+
     if len(dataList) == 0:
         update.callback_query.edit_message_text(dataString, parse_mode="HTML")
         update.callback_query.message.reply_html(
---
+++ helpTexts.py
@@ -0,0 +1,27 @@
+helpText = f'''This Bot🤖 will send you Timetable of your course
+
+<b>How to Get All Timetable of my Course?</b>
+First Send /start command to bot🤖
+After that follow instructions
+1️⃣ Select Your University 🎓
+2️⃣ Select Your Semester 📚
+3️⃣ Select Your Course 📖
+✅ You'll Receive Timetables
+
+<b>How to Get Single Subject Date and Time?</b>
+To get perticular subject date and time use /qpcode command
+
+1️⃣Send /qpcode with your subject QPCode
+    EX - /qpcode 12345
+    type /qpcode give a single space then type QPCode and send
+2️⃣This will reply you Date and Time of that Subject
+✅Done
+
+If you have any question contact us by using /contact command[You'll get contact details]
+
+➡️ Kindly Report if you have any issues regarding 🤖bot as well as the Data 🤖bot returned
+➡️ Don't follow blindly Bot🤖 given data kindly cross verify with the timetable in university website🌐
+'''
+
+
+footer = f"<b>🤝All The Best for your Exams📚</b>\n<b>👏Do Well in All the Exams 📚</b>\n\n\n\n🔗Connect With Us\n\n\nFor Study Materials 📖 Check<a href='https://bustudymate.in'> BUStudymate Blog</a>\n\nFor Instant Updates📢 Follow On <a href = 'https://instagram.com/bustudymate'>Instagram🖼️</a>\n\nFor 👩‍💻Discussion/Q & A⁉️ - <a href='https://forum.bustudymate.in'>Join BUForum</a>\n\nFeedbacks👏/ Report❌ Email ✉️ - admin@bustudymate.in\n\nDonate💵 to Run the Service - @bustudymate4u"
