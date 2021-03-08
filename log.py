b3579792989f6ad83584a814f035b0f18cb47abe
Varun Deva <varundev23@gmail.com>
2021-03-08 17:36:39 +0530

    bug fix

--- log.py
+++ log.py
@@ -1,4 +1,8 @@
 import os
+from datetime import datetime
+
+now = datetime.now()
+dateTime = now.strftime("%d/%m/%Y %H:%M:%S")
 
 
 def addToLog(context, update):
@@ -7,7 +11,8 @@ Checked Time Table\n
 User ID - {update.callback_query.message.chat.id}
 UserName - @{update.callback_query.message.chat.username}
 User Name - {update.callback_query.message.chat.first_name} {update.callback_query.message.chat.last_name}
-Date Time - {update.callback_query.message.date}\n
+Date Time (Telegram Server)- {update.callback_query.message.date}\n
+Current Date Time - {dateTime}
 Call Back Query Data - {context.user_data}
 '''
     print(template)
@@ -22,8 +27,9 @@ User ID - {update.message.chat.id}
 UserName - @{update.message.chat.username}
 User Name - {update.message.chat.first_name} {update.message.chat.last_name}
 User Status in Channel - {status}
-Date Time - {update.message.date}\n
-    '''
+Date Time(Telegram Server) - {update.message.date}
+Current Date Time - {dateTime}
+'''
     print(template)
     context.bot.send_message(
         chat_id=os.environ.get("LOG_CHANNEL"), text=template)
